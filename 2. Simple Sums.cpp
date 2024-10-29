cba32074f1a9b0a8d5277efee32720ed7de166ed
GSav2468 <Gurdave.Singh24@bathspa.ac.uk>
2024-10-29 16:53:16 +0000

    Undate
    Updated

---
+++ 2. Simple Sums.cpp
@@ -0,0 +1,22 @@
+// Simple Sums.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}int myInt = 5;
+double myFirstDouble = 1.23, mySecondDouble = 4.56;
+char myChar = 'c';
+bool myBool = false;
+
+int myInt = 10;
+double myFirstDouble = 1.235, mySecondDouble;
+
+myInt = 6;
+myFirstDouble = 2.34;
+mySecondDouble = 1.65;
+
+int Total = 10;
+
